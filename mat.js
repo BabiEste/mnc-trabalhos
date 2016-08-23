68c7bb1f8627d885319919c7759559064bf4ade2
Luis Uzai <luisuzai13@gmail.com>
2016-08-23 00:01:58 -0300

    Adicionado o gráfico

--- mat.js
+++ mat.js
@@ -5,13 +5,20 @@ app.controller('myCtrl', function($scope) {
     $scope.x = '';
     $scope.calcular = function() {
         $scope.resultado = math.eval($scope.expression,{x: Number($scope.x)});
-    }
-});
+        try {
+          functionPlot({
+            target: '#plot',
+            data: [{
+              fn: $scope.expression,
+              sampler: 'builtIn',  // this will make function-plot use the evaluator of math.js
+              graphType: 'polyline'
+            }]
+          });
+        }
+        catch (err) {
+          console.log(err);
+          alert(err);
+        }
 
-// var expression = document.getElementById('expression');
-// var evaluate   = document.getElementById('evaluate');
-// var result     = document.getElementById('result');
-// var x     = document.getElementById('x').value;
-// evaluate.onclick = function () {
-//   result.innerHTML = math.eval(expression.value,{x: Number(x)});
-// };
+      }
+    });
