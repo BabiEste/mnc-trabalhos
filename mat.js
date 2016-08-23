92b9398694a951a2e7ca07102fd6e1a4f99a59b5
Luis Uzai <luisuzai13@gmail.com>
2016-08-22 22:50:07 -0300

    Funcionando com o Angular

--- mat.js
+++ mat.js
@@ -1,12 +1,11 @@
 var app = angular.module('myApp', []);
+
 app.controller('myCtrl', function($scope) {
-  // // $scope.math = math.eval();
-  // $scope.resultado = functio($scope.expression, $scope.x){
-  //   x2 = $scope.x;
-  //   exp = $scope.expression;
-$scope.resultado = math.eval($scope.expression.value,{x: Number($scope.x)});
-  //   return math.eval(exp.value, {x2:Number(x2)});
-  }
+    $scope.expression = '';
+    $scope.x = '';
+    $scope.calcular = function() {
+        $scope.resultado = math.eval($scope.expression,{x: Number($scope.x)});
+    }
 });
 
 // var expression = document.getElementById('expression');
